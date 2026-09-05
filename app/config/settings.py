562172c380db4506858e9f9cd7de467ec50bca63
Aditya <aditya0dagar@gmail.com>
2026-09-05 11:55:56 +0530

    database test

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,10 +1,22 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 class Settings(BaseSettings):
     app_name: str = "Antarctic Digital Twin"
     environment: str = "dev"
     secret_key: str = "super-secret-key-change-in-prod"
-    mysql_dsn: str = "mysql+asyncmy://root:password@localhost:3306/dtfias"
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
+class ENV():
+    class database():
+        db_user: str = os.getenv("DB_USER")
+        db_password: str = os.getenv("DB_PASSWORD")
+        db_host: str = os.getenv("DB_HOST")
+        db_port: int = os.getenv("DB_PORT")
+        db_name: str = os.getenv("DB_NAME")
+
+
 settings = Settings()
+env = ENV()
